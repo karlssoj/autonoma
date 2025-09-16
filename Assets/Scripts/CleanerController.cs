b98e1eaf797f596b432c1c93fcf808341fe5ab92
karlssoj <karlssoj@gmail.com>
2025-09-16 15:58:14 +0300

    First working

--- Assets/Scripts/CleanerController.cs
+++ Assets/Scripts/CleanerController.cs
@@ -8,6 +8,7 @@ public class CleanerController : Agent
     public float TurnSpeed = 10;
     Vector3 StartPosition;
     public GameObject Dust;
+    float PreviousDistance = 0;
 
 
 
@@ -20,8 +21,24 @@ public class CleanerController : Agent
 	//(om man kör simulationen i människostyrt läge)     
     public override void OnActionReceived(ActionBuffers actions)
     {
-		//En Array med disreta actions. Två element med värde 0, 1, eller 2
-		//Matchar med de actions man definierat i Beahiour Parameters skriptet
+        //Nuvarande avstånd till skräp
+        float CurrentDistance = Vector3.Distance(transform.localPosition, Dust.transform.localPosition);
+
+        //Föregående asvstånd minus nuvarande avstånd blir positivt om vi kommit närmare skräpet
+        float Delta = PreviousDistance - CurrentDistance;
+
+        if(PreviousDistance != 0)
+            //Pluspoäng om vi kommit närmare skräpet, minuspoäng om vi gått längre bort från skräpet
+            AddReward(Delta * 0.01f);
+
+        //Uppdaterar previousDistance
+        PreviousDistance = CurrentDistance;
+
+        //Varje beslut bör "kosta" lite, detta för att lära agenten att så snabbt som möjligt hitta skräpet
+        AddReward(-0.0001f);
+
+        //En Array med disreta actions. Två element med värde 0, 1, eller 2
+        //Matchar med de actions man definierat i Beahiour Parameters skriptet
         var action = actions.DiscreteActions;
 
         if (action[0] == 1)
